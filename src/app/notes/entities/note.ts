736b9b2a9d544ac1cba5bf9bc13d0863851af763
andriislipenko <andrii.slipenko@gmail.com>
2020-07-13 19:01:54 +0300

    minor entitiy fix

--- src/app/notes/entities/note.ts
+++ src/app/notes/entities/note.ts
@@ -4,4 +4,18 @@ export class Note {
     text: string = '';
     timestamp: Date;
     done: boolean = false;
+
+    constructor (
+        id: number,
+        title: string = '',
+        text: string = '',
+        timestamp: Date | string,
+        done: boolean = false
+    ) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.timestamp = timestamp instanceof Date ? timestamp : new Date(timestamp);
+        this.done = done;
+    }
 }
